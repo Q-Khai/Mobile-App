efa03a54c95e2116150dee8566c7dfd1f844edd5
TrinhNgocBaoBaro <105273438+TrinhNgocBaoBaro@users.noreply.github.com>
2023-02-23 01:43:48 +0700

    Authen send Token

--- demo/lib/auth_service.dart
+++ demo/lib/auth_service.dart
@@ -1,10 +1,11 @@
+import 'dart:convert';
 import 'package:demo/hello_page.dart';
 import 'package:demo/home_page.dart';
 import 'package:demo/page/login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-// import 'dart:html';
+import 'package:http/http.dart' as http;
 
 class AuthService {
   handleAuthState() {
@@ -31,15 +32,40 @@ class AuthService {
       accessToken: googleAuth.accessToken,
       idToken: googleAuth.idToken,
     );
+
+
     UserCredential userCredential =
         await FirebaseAuth.instance.signInWithCredential(credential);
-    // print(userCredential.user?.displayName);
-    // print(credential.accessToken);
-    // print(userCredential.user?.email);
+
+       print(userCredential.user?.getIdToken().then((value) => print(value)));
+
+       var accessToken = userCredential.user?.getIdToken().then((value) => sendToken(value));
+       print(3);
+       sendToken(accessToken);
+      // FirebaseAuth.instance.signInWithPopup(credential);
+
     return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 
- Future<void> signOut() async {
+  sendToken(accessToken) async {
+    final url = Uri.parse('https://ec2-3-0-97-134.ap-southeast-1.compute.amazonaws.com:8080/login');
+    var response = await http.post(
+      url, 
+      body: jsonEncode({'idToken': accessToken}),
+      headers: {'Content-Type': 'application/json'},
+      );
+    
+
+if (response.statusCode == 200) {
+      print('Access successfully!');
+       print(response.body);
+    } else {
+      print('Access failed with status code ${response.statusCode}');
+    }
+
+  }
+
+  Future<void> signOut() async {
     final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
     final GoogleSignIn _googleSignIn = GoogleSignIn();
     await _firebaseAuth.signOut();
